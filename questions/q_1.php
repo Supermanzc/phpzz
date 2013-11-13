dbab2f9266e1f64047b04f04f3444d86b160a7d7
ZhongChuan <bianjiyuyan@gmail.com>
2013-11-13 17:57:30 +0800

    zc

--- questions/q_1.php
+++ questions/q_1.php
@@ -6,4 +6,9 @@
  * Time: 下午2:58
  */
 header("Content-type: text/html; charset=utf-8");
-echo strip_tags('<a href=#>你好吗</a>');
+$string = "April 15, 2003";
+$pattern = "/(\w+) (\d+), (\d+)/i";
+$replacement = "\${1}1,\$3";
+print preg_replace($pattern, $replacement, $string) . '<br />';
+$d = dir(dirname(__FILE__));
+print_r($d);
