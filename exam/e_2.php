0ebb3e343551b9c70b3e5ebba71242d81c078eb2
ZhongChuan <bianjiyuyan@gmail.com>
2013-11-19 09:43:30 +0800

    zc

---
+++ exam/e_2.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 13-11-19
+ * Time: 上午9:33
+ */
+$connect = mysql_connect('localhost:3306', 'root', '123456')or die('数据库连接失败:'.mysql_error());
