1d0a458b4ea707e8245234e20802e2d72c4cc9d6
zc <zc@email.com>
2013-12-27 15:04:53 +0800

    zc

--- exam/e_2.php
+++ exam/e_2.php
@@ -7,5 +7,6 @@
  */
 $connect = mysql_connect('localhost:3306', 'root', '123456')or die('数据库连接失败:'.mysql_error());
 
-echo md5('1235466');
+//echo md5('1235466');
 echo strlen(md5('1235466'));
+echo md5('admin');
